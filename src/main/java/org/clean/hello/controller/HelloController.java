cfdd86e2f809aa67bea68ba3f9e1221b37f51dc7
Yucel Cekinmez <cekinmezyucel@gmail.com>
2022-07-08 13:29:25 +0200

    first commit

---
+++ src/main/java/org/clean/hello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.clean.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello!";
+    }
+
+}
